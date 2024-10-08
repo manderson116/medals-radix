bd0bd793e424ac591f8189002bec5d60c586b5e6
jgrissom <jgrissom91@gmail.com>
2024-10-07 21:27:21 -0500

    [1.06] Embed jsx expressions using state data

--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,5 +1,15 @@
+import { useState } from "react";
+
 function Country() {
-  return <h2>United States</h2>;
+  const [name, setName] = useState("United States");
+  const [gold, setGold] = useState(0);
+
+  return (
+    <>
+      <h2>{name}</h2>
+      <div>Gold medals: {gold}</div>
+    </>
+  );
 }
 
 export default Country;

d4a6804b9a65c5b5f337e3770e7450c274f88da7
jgrissom <jgrissom91@gmail.com>
2024-10-07 22:07:29 -0500

    [2.01a] Passing data to components - json array 🚀 🐧 Create Country components with the array.map function 🐧 Assign unique key

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,18 @@ import Country from "./components/Country";
 import "./App.css";
 
 function App() {
-  return <Country />;
+  const [countries, setCountries] = useState([
+    { id: 1, name: "United States", gold: 2 },
+    { id: 2, name: "China", gold: 3 },
+    { id: 3, name: "France", gold: 0 },
+  ]);
+  return (
+    <div className="countries">
+      {countries.map((country) => (
+        <Country key={country.id} />
+      ))}
+    </div>
+  );
 }
 
 export default App;

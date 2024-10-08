572fd3ac5aba48d2e38116c766bb0e98843a4370
jgrissom <jgrissom91@gmail.com>
2024-10-08 15:42:58 -0500

    [3a.05] Display total medals awared to country

--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,10 +1,19 @@
 import Medal from "./Medal";
 
 function Country(props) {
+  function getMedalsTotal() {
+    let sum = 0;
+    props.medals.forEach((medal) => {
+      sum += props.country[medal.name];
+    });
+    return sum;
+  }
+
   return (
     <div className="country">
       <div className="header">
         <h2>{props.country.name}</h2>
+        <div>{getMedalsTotal()}</div>
         <div
           className="basket"
           onClick={() => props.onDelete(props.country.id)}

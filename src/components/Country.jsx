dfe6e0548e892b5a28394254c1ebd13fe8150c56
jgrissom <jgrissom91@gmail.com>
2024-10-07 21:42:20 -0500

    [1.07] Handle click event 🚀 🐧 increment gold medal count

--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -4,12 +4,16 @@ function Country() {
   const [name, setName] = useState("United States");
   const [gold, setGold] = useState(0);
 
+  function handleClick() {
+    setGold(gold + 1);
+  }
+
   return (
     <div className="country">
       <h2>{name}</h2>
       <div>
         Gold medals: {gold}
-        <button>+</button>
+        <button onClick={handleClick}>+</button>
       </div>
     </div>
   );

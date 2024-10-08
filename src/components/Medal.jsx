4736b321d966ce6a417c711fe9f631b260e37265
jgrissom <jgrissom91@gmail.com>
2024-10-08 15:49:22 -0500

    [3a.07] Create increment / decrement buttons

--- src/components/Medal.jsx
+++ src/components/Medal.jsx
@@ -2,6 +2,10 @@ function Medal(props) {
   return (
     <div className="medals">
       {props.country[props.medal.name]} {props.medal.name} medals
+      <div className="button">
+        <button>-</button>
+        <button>+</button>
+      </div>
     </div>
   );
 }

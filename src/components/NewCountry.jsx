7627faf7f7e7d86153a61298fd363b16e5e7e449
jgrissom <jgrissom91@gmail.com>
2024-10-21 19:41:15 -0500

    [4.02] Toggle dialog using state

--- src/components/NewCountry.jsx
+++ src/components/NewCountry.jsx
@@ -1,9 +1,22 @@
+import { useState } from "react";
 import trigger from "../assets/trigger.svg";
 
 function NewCountry() {
+  const [showDialog, setShowDialog] = useState(false);
+
   return (
     <>
-      <img src={trigger} alt="New Country" title="New Country" id="trigger" />
+      {showDialog ? (
+        <form>dialog here</form>
+      ) : (
+        <img
+          src={trigger}
+          alt="New Country"
+          title="New Country"
+          id="trigger"
+          onClick={() => setShowDialog(true)}
+        />
+      )}
     </>
   );
 }

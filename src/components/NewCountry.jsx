1590029c4f4812edf314507203fea63da8675e1d
jgrissom <jgrissom91@gmail.com>
2024-10-21 19:41:25 -0500

    [4.03] NewCountry Form

--- src/components/NewCountry.jsx
+++ src/components/NewCountry.jsx
@@ -7,7 +7,27 @@ function NewCountry() {
   return (
     <>
       {showDialog ? (
-        <form>dialog here</form>
+        <form>
+          <div id="overlay"></div>
+          <div id="dialog">
+            <h3>Enter new country name</h3>
+            <input
+              type="text"
+              name="NewCountryName"
+              placeholder="enter name"
+              autoFocus
+              autoComplete="off"
+            />
+            <div>
+              <button type="submit" id="save">
+                save
+              </button>
+              <button id="cancel" type="button">
+                cancel
+              </button>
+            </div>
+          </div>
+        </form>
       ) : (
         <img
           src={trigger}
